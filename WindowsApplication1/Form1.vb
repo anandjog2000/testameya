b6ee20ab5063f602ee7181115f667c53da07bd84
testanand <anandjog2000@yahoo.com>
2016-06-03 16:37:24 +0530

    gdgdf

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,6 +1,9 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        MsgBox("Hi111")
+        MsgBox("Hi111fsfs")
+        Dim i As Integer
+        i = 10
+
     End Sub
 End Class
